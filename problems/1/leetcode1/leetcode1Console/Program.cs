bcf08e5fc9cf7da3caf2a48aceb00340c64b8913
Pavel Baykov <ppbaykov@gmail.com>
2023-02-09 22:26:16 +0300

    leetcode1_tmp

---
+++ problems/1/leetcode1/leetcode1Console/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using leetcode1Console;
+
+var decision = new Solution();
+var input = new int[] { 3,2,4 };
+var twoDigits = decision.TwoSum(input, 6);
+
+Console.WriteLine(twoDigits[0]+twoDigits[1]);
---
+++ problems/1/leetcode1/leetcode1Console/Solution.cs
@@ -0,0 +1,18 @@
+namespace leetcode1Console;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (var i = 0; i< nums.Length-1; i++)
+        {
+            for (var j = i+1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                    return new [] {i, j};
+            }
+        }
+
+        throw new Exception("bad input");
+    }
+}
